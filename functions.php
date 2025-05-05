0991d4331f4187b030c5f9a1ab075528b7818f20
sanjay-np <excelblade10@gmail.com>
2025-05-05 13:23:36 +0545

    chore: added tailwind support

--- functions.php
+++ functions.php
@@ -3,4 +3,4 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 //Load Theme Initialization
-require_once __DIR__ . '/src/cloud-theme.php';
+require_once __DIR__ . '/inc/cloud-theme.php';
--- template-parts/header.php
+++ template-parts/header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <?php wp_head(); ?>
 </head>
 
